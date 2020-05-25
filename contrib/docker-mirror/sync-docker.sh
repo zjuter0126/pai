a0d53d297745e7ecf2e5c0766b0d4dc63a261ca7
hzy46 <362583303@qq.com>
2020-05-25 14:23:30 +0800

    fix

--- contrib/docker-mirror/sync-docker.sh
+++ contrib/docker-mirror/sync-docker.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+set -e
+
+if [ $# -ne 1 ]; then
+  echo "Usage: bash -x sync-docker.sh <mirror-image-account>"
+  exit 1
+else
+  account=$1
+fi
+
 echo \
 'gcr.io/google_containers/pause-amd64:3.1
 gcr.io/google-containers/k8s-dns-node-cache:1.15.5
@@ -7,6 +34,12 @@ gcr.io/google_containers/kube-registry-proxy:0.4
 gcr.io/google_containers/metrics-server-amd64:v0.3.3
 k8s.gcr.io/addon-resizer:1.8.3
 gcr.io/google_containers/kubernetes-dashboard-amd64:v1.10.1' \
-| while read repo; do
-  echo $repo;
+| while read image; do
+  image_name=`echo $image | cut -d "/" -f2- | cut -d ":" -f1`
+  image_name=${name//\//-}
+  tag=`echo $image | cut -d ":" -f2`
+  new_image="${account}/${image_name}:${tag}"
+  docker pull $image
+  docker tag $image $new_image
+  docker push ${new_image}
 done
