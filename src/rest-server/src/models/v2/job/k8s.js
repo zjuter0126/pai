6978bfa9f83a591335c3718db7670fd26776b87a
YundongYe <YundongYe@outlook.com>
2020-07-03 11:28:08 +0800

    example commit for stress test

--- src/rest-server/src/models/v2/job/k8s.js
+++ src/rest-server/src/models/v2/job/k8s.js
@@ -414,6 +414,14 @@ const generateTaskRole = (frameworkName, taskRole, jobInfo, frameworkEnvList, co
           privileged: false,
           restartPolicy: 'Never',
           serviceAccountName: 'runtime-account',
+          tolerations: [
+              {
+                key: "virtual-kubelet.io/provider",
+                operator: "Equal",
+                value: "mock",
+                effect: "NoSchedule",
+              }
+          ],
           initContainers: [
             {
               name: 'init',
@@ -462,7 +470,6 @@ const generateTaskRole = (frameworkName, taskRole, jobInfo, frameworkEnvList, co
                 limits: {
                   'cpu': config.taskRoles[taskRole].resourcePerInstance.cpu,
                   'memory': `${config.taskRoles[taskRole].resourcePerInstance.memoryMB}Mi`,
-                  'github.com/fuse': 1,
                   'nvidia.com/gpu': config.taskRoles[taskRole].resourcePerInstance.gpu,
                   ...infinibandDevice && {'rdma/hca': 1},
                 },
